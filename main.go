7f582d6a338c01824cf6768d5bd36ef302702569
asidikfauzi <asidikfauzi@gmail.com>
2025-01-19 04:14:39 +0700

    ok

--- main.go
+++ main.go
@@ -5,4 +5,5 @@ import "fmt"
 func main() {
 	fmt.Println("Selamat, anda berhasil clone project ini.")
 	fmt.Println("Selamat, anda berhasil clone project ini.")
+	fmt.Println("Selamat, anda berhasil clone project ini.")
 }
